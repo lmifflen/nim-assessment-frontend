2eb12e3f97878b80619517fc8a05448e097f6533
lmifflen <lmifflen@gmail.com>
2022-12-01 10:45:15 -0700

    Confirmation Page

--- src/components/ConfirmationPage.jsx
+++ src/components/ConfirmationPage.jsx
@@ -1,35 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router';
-import OrderConfirmation from './OrderConfirmation';
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import OrderConfirmation from "./OrderConfirmation";
 
 function ConfirmationPage() {
-    const [confirmation, setConfirmation] = useState({name: "", address: "", items: [], id: ""});
-    // const [name, setName] = useState("");
-    // const [address, setAddress] = useState("");
-    // const [items, setItems] = useState([]);
+  const [confirmation, setConfirmation] = useState({
+    name: "",
+    address: "",
+    items: [],
+    id: ""
+  });
+  // const [name, setName] = useState("");
+  // const [address, setAddress] = useState("");
+  // const [items, setItems] = useState([]);
 
-    const id = useParams();
-    const getConfirmation = async () => {
-        console.log(id);
-        const response = await fetch(`/api/orders/${id.id}`);
-        const data = await response.json();
+  const id = useParams();
+  const getConfirmation = async () => {
+    // console.log(id);
+    const response = await fetch(`/api/orders/${id.id}`);
+    const data = await response.json();
 
-        console.log(data);
-        setConfirmation(data);
-        // setAddress(data.address);
-        // setName(data.name);
-        // setItems(data.items);
+    // console.log(data);
+    setConfirmation(data);
+    // setAddress(data.address);
+    // setName(data.name);
+    // setItems(data.items);
+  };
 
-      };
+  useEffect(() => {
+    getConfirmation();
+  }, []);
 
-      useEffect(() => {
-        getConfirmation();
-        }, []);
-   
   return (
-    <div><OrderConfirmation confirmation={confirmation} /></div>
-  )
+    <div>
+      <OrderConfirmation confirmation={confirmation} />
+    </div>
+  );
 }
 
-export default ConfirmationPage
+export default ConfirmationPage;
